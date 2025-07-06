d4cbf2c0b556b9ea335b89339adbc4e98b0556f1
mmjustme <infooneuser@gmail.com>
2025-07-06 22:16:36 +0300

    updated: Added basic styles and structure

--- index.php
+++ index.php
@@ -6,10 +6,48 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/assets/css/main.css">
-<!--    <link rel="stylesheet" href="/assets/css/styles.css">-->
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
     <title>BLOG-APP</title>
 </head>
 <body>
-<h1>Hello World</h1>
+
+
+<header class="header">
+    <nav class="nav">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="container">
+    <main class="main">
+        <div class="main-content">
+            <div class="card">
+                <h3 class="card-title">Card title</h3>
+                <p class="card-content">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+
+        </div>
+        <div class="main-sidebar">sidebar</div>
+    </main>
+
+    <footer class="footer">
+        <div class="text">
+            <p>&copy; Copyright <?= date('Y') ?> </p>
+        </div>
+    </footer>
+</div>
 </body>
 </html>
