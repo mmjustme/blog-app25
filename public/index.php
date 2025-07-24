5a599ab3268696a95c103b32d2ba0a5ea437dbb5
mmjustme <infooneuser@gmail.com>
2025-07-24 21:59:18 +0300

    updated: index.php. Added new way of uri parameters, .htaccess has described about QSA tag

--- public/index.php
+++ public/index.php
@@ -4,8 +4,11 @@ require_once dirname(__DIR__) . "/config/paths.php";
 
 require_once CORE . "/helper.php";
 
-$uri = trim($_SERVER['REQUEST_URI'], "/");
-//dd($uri);
+$parsedUri = parse_url($_SERVER["REQUEST_URI"]);// розбираємо адресну строку на масив( в якому шлях і квері)
+$uri = trim($parsedUri["path"], "/"); // about
+//dump($uri);
+dump($_GET);
+//dump($_SERVER["QUERY_STRING"]);
 
 switch ($uri) {
     case '':
